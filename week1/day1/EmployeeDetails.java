9fe52eb92019cd2dad778c66ad840a682ede6881
vinodha2106 <vinodha2106@gmail.com>
2025-10-04 13:31:31 +0530

    Add files via upload
    Week1-Day1-Assignments

---
+++ week1/day1/EmployeeDetails.java
@@ -0,0 +1,32 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String empName="John Doe";
+		int empAge=30;
+		double empSal=55000.75;
+		char empGen='M';
+		boolean acceptTerms=true;
+		float empRating=4.5f;
+		long empNumber=9876543210l;
+		int empPincode=56001;
+		int empLucNum=7;
+		
+		System.out.println("Employee Details of John Doe ");
+		System.out.println("Employee Name: " +empName);
+		System.out.println("Employee Age: " +empAge);
+		System.out.println("Employee salary:" +empSal);
+		System.out.println("Employee Gender: "+empGen);
+		System.out.println("Employee accepted terms and condition :" +acceptTerms);
+		System.out.println("Employee Rating "+empRating);
+		System.out.println("Employee Contact Number:" +empNumber);
+		System.out.println("Employee pincode:" +empPincode);
+		System.out.println("Employee luckynumber: " +empLucNum);
+	
+		
+	
+	
+	}
+}
---
+++ week1/day1/Firefox.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double browserVersion=100.2;
+		String browserName="Firefox";
+		int releaseYear=1998;
+		boolean isVisible=true;
+		char browserLogo='f';
+		System.out.println("Browser version is:"+browserVersion);
+		System.out.println("Browser name is:"+browserName);
+		System.out.println("Browser releaseYear is :"+releaseYear);
+		System.out.println("Browser visible:"+isVisible);
+		System.out.println("Browser Logo:"+browserLogo);
+	}
+
+}
